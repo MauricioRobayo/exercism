ca15f391bee48588f8ff642fd3b467c454512a09
Mauricio Robayo <rfmajo@gmail.com>
2020-06-24 21:39:26 -0500

    Updated gigaseconds solution

--- typescript/gigasecond/gigasecond.ts
+++ typescript/gigasecond/gigasecond.ts
@@ -1,9 +1,12 @@
+const GIGASECOND = 1_000_000_000
+
 class Gigasecond {
   constructor(private d: Date) {
   }
   date(): Date {
     const d2 = new Date(this.d)
-    return new Date(d2.setSeconds(this.d.getSeconds() + 1000000000));
+    const gigamilliseconds = GIGASECOND * 1000
+    return new Date(d2.setTime(this.d.getTime() + gigamilliseconds))
   }
 }
 
