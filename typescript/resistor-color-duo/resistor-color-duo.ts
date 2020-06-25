2dbd8accef811c2c650a840e46486b4e104c820f
Mauricio Robayo <rfmajo@gmail.com>
2020-06-24 20:34:35 -0500

    Improve resistor color duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,19 +1,33 @@
+type Color =
+  | "black"
+  | "brown"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "violet"
+  | "grey"
+  | "white"
+
+
 export class ResistorColor {
 
-  private colorMap: Record<string, string> = {
-    black: "0",
-    brown: "1",
-    red: "2",
-    orange: "3",
-    yellow: "4",
-    green: "5",
-    blue: "6",
-    violet: "7",
-    grey: "8",
-    white: "9",
+
+  private colorMap: Record<Color, number> = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
   }
 
-  constructor(private colors: string[]) {
+  constructor(private colors: Color[]) {
     if (this.colors.length < 2) {
       throw new Error("At least two colors need to be present")
     }
