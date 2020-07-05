3ee5f29d8bea146aa54ab9ba2bae2f205d4a6bfa
Mauricio Robayo <rfmajo@gmail.com>
2020-07-04 21:00:15 -0500

    Add word-count solution

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,7 +1,18 @@
+type wordFrequency = Map<string, number>
+
 class Words {
-  count(phrase: string) {
-    const words: string[] = phrase.split('')
-    const wordsFrequency: Record<string, number> = {}
-    words
+  count(phrase: string): wordFrequency {
+    const wordsFrequency: wordFrequency = new Map()
+    phrase
+      .trim()
+      .split(/\s+/)
+      .forEach((word) => {
+        const normalizedWord = word.toLowerCase()
+        const wordFreq = wordsFrequency.get(normalizedWord)
+        wordsFrequency.set(normalizedWord, wordFreq ? wordFreq + 1 : 1)
+      })
+    return wordsFrequency
   }
 }
+
+export default Words
