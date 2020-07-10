0d97a6743b7be4a75e62c1fe486db1e617b2d85e
Mauricio Robayo <rfmajo@gmail.com>
2020-07-10 08:40:54 -0500

    improve gigasecond solution

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,7 +1,6 @@
-
+const GIGASECOND = 1e9
 
 export const gigasecond = (date) => {
-  const GIGASECOND = 1_000_000_000
   const gigamillisecond = GIGASECOND * 1000
 
   return new Date(date.getTime() + gigamillisecond)
