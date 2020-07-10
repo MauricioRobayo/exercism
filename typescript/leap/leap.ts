6c2ce88f9f7123181db0702f45b96939701dd3a8
Mauricio Robayo <rfmajo@gmail.com>
2020-07-10 08:29:01 -0500

    make leap year condition a oneliner

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,17 +1,7 @@
 function isLeapYear(year: number): boolean {
-    if (year % 4 !== 0) {
-        return false
-    }
-       
-    if (year % 100 !== 0) {
-        return true
-    }
 
-    if (year % 400 === 0) {
-        return true
-    }
-    
-    return false
+    return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0) 
+
 }
 
 export default isLeapYear
