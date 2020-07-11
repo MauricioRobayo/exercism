13455c82cda3a5fc92515df460949df04a8f7e94
Mauricio Robayo <rfmajo@gmail.com>
2020-07-10 21:00:25 -0500

    Make leap more expressive with classes

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,7 +1,17 @@
-function isLeapYear(year: number): boolean {
+class Year {
+  constructor(private value: number) {}
 
-    return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0) 
-
+  isDivisibleBy(divisor: number): boolean {
+    return this.value % divisor === 0;
+  }
 }
 
-export default isLeapYear
+function isLeapYear(n: number): boolean {
+  const year = new Year(n);
+  return (
+    (year.isDivisibleBy(4) && !year.isDivisibleBy(100)) ||
+    year.isDivisibleBy(400)
+  );
+}
+
+export default isLeapYear;
