ae1c5e73b11ea48bcb81fa07d2600371d0f9c039
Mauricio Robayo <rfmajo@gmail.com>
2020-07-18 20:00:35 -0500

    Improve rna-transcription solution

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,24 +1,23 @@
-type DNA = 'G' | 'C' | 'T' | 'A';
-type RNA = 'C' | 'G' | 'A' | 'U';
+type DNA = "G" | "C" | "T" | "A";
+type RNA = "C" | "G" | "A" | "U";
 
 class Transcriptor {
   toRna(dna: string): string {
     const complements: Record<DNA, RNA> = {
-      G: 'C',
-      C: 'G',
-      T: 'A',
-      A: 'U',
+      G: "C",
+      C: "G",
+      T: "A",
+      A: "U",
+    };
+
+    const dnaArray = [...dna];
+
+    if (!dnaArray.every((e) => Object.keys(complements).includes(e))) {
+      throw new Error("Invalid input DNA.");
     }
-    return [...dna]
-      .map((e: DNA) => {
-        if (complements[e] === undefined) {
-          throw new Error('Invalid input DNA.')
-        }
-        return complements[e];
-      })
-      .join("");
+
+    return (<DNA[]>dnaArray).map((e) => complements[e]).join("");
   }
 }
 
 export default Transcriptor;
-w
