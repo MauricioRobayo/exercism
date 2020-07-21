1fb35d6ef2d50f81c9f7c5ac109ce9cfaf3ed1d1
Mauricio Robayo <rfmajo@gmail.com>
2020-07-20 20:11:24 -0500

    Add collatz-conjecture recursive solution

---
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -0,0 +1,15 @@
+class CollatzConjecture {
+    static steps(n: number): number {
+        if (n < 1) {
+            throw new Error('Only positive numbers are allowed')
+        }
+
+        if (n === 1) {
+            return 0;
+        }
+
+        return CollatzConjecture.steps(n % 2 === 0 ? n / 2 : n * 3 + 1) + 1;
+    }
+}
+
+export default CollatzConjecture
