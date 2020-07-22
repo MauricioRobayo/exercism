25ae95613f9631cee4b11e7ce81833787f9d6ff6
Mauricio Robayo <rfmajo@gmail.com>
2020-07-21 19:50:28 -0500

    Use type guards to avoid casting

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -2,23 +2,28 @@ type DNA = "G" | "C" | "T" | "A";
 type RNA = "C" | "G" | "A" | "U";
 
 class Transcriptor {
+  readonly complements: Record<DNA, RNA> = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U",
+  };
+
   toRna(dna: string): string {
-    const complements: Record<DNA, RNA> = {
-      G: "C",
-      C: "G",
-      T: "A",
-      A: "U",
-    };
     let rna = '';
 
-    for (const dnaStrand of dna){
-      if (!(dnaStrand in complements)) {
+    for (const nucleotide of dna){
+      if (!this.isDnaNucleotide(nucleotide)) {
         throw new Error("Invalid input DNA.");
       }
-      rna += complements[<DNA>dnaStrand];
+      rna += this.complements[nucleotide];
     }
 
     return rna;
+  }
+
+  isDnaNucleotide(nucleotide: DNA | string): nucleotide is DNA {
+    return nucleotide in this.complements;
   }
 }
 
