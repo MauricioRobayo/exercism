599f6def7d3d4a77820e5e1df32b6a4688c15303
Mauricio Robayo <rfmajo@gmail.com>
2020-07-31 19:25:15 -0500

    Improve space-age.ts solution

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,72 +1,73 @@
 type Planet =
-  | 'mercury'
-  | 'venus'
-  | 'earth'
-  | 'mars'
-  | 'jupiter'
-  | 'saturn'
-  | 'uranus'
-  | 'neptune'
+  | "mercury"
+  | "venus"
+  | "earth"
+  | "mars"
+  | "jupiter"
+  | "saturn"
+  | "uranus"
+  | "neptune";
 
 const orbitalPeriodsRelativeToEarth: Record<Planet, number> = {
-  mercury:  0.2408467,
+  mercury: 0.2408467,
   venus: 0.61519726,
   earth: 1,
   mars: 1.8808158,
   jupiter: 11.862615,
   saturn: 29.447498,
   uranus: 84.016846,
-  neptune:164.79132,
-}
-
+  neptune: 164.79132,
+};
 
 class SpaceAge {
   private EARTH_ORBITAL_PERIOD = 31557600;
   public readonly seconds: number;
   constructor(seconds: number) {
-    this.seconds = seconds
+    this.seconds = seconds;
   }
 
   onEarth(): number {
-    return this.getAgeInYears('earth');
+    return this.getAgeInYears("earth");
   }
 
   onMercury(): number {
-    return this.getAgeInYears('mercury');
+    return this.getAgeInYears("mercury");
   }
 
   onVenus(): number {
-    return this.getAgeInYears('venus');
+    return this.getAgeInYears("venus");
   }
 
   onMars(): number {
-    return this.getAgeInYears('mars');
+    return this.getAgeInYears("mars");
   }
 
   onJupiter(): number {
-    return this.getAgeInYears('jupiter');
+    return this.getAgeInYears("jupiter");
   }
 
   onSaturn(): number {
-    return this.getAgeInYears('saturn');
+    return this.getAgeInYears("saturn");
   }
 
   onUranus(): number {
-    return this.getAgeInYears('uranus');
+    return this.getAgeInYears("uranus");
   }
 
   onNeptune(): number {
-    return this.getAgeInYears('neptune');
+    return this.getAgeInYears("neptune");
   }
 
   private getAgeInYears(planet: Planet): number {
-    return this.roundToTwoDecimals(
-      this.seconds / this.EARTH_ORBITAL_PERIOD / orbitalPeriodsRelativeToEarth[planet]
-    );
+    const ageInYears =
+      this.seconds /
+      this.EARTH_ORBITAL_PERIOD /
+      orbitalPeriodsRelativeToEarth[planet];
+    return this.round(ageInYears, 2);
   }
 
-  private roundToTwoDecimals(n: number): number {
-    return Math.round(n * 100) / 100;
+  private round(n: number, decimals: number): number {
+    return Number(n.toFixed(decimals));
   }
 }
 
