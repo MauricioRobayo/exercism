502fd9df860d1e34a317a552148be2a69d78d2be
Mauricio Robayo <rfmajo@gmail.com>
2020-08-10 20:33:55 -0500

    Use array instead of object

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,14 +1,14 @@
-const factors = [
-  { factor: 3, name: 'Pling' },
-  { factor: 5, name: 'Plang' },
-  { factor: 7, name: 'Plong' },
-]
+const factors: string[] = [];
+factors[3] = 'Pling';
+factors[5] = 'Plang';
+factors[7] = 'Plong';
 
 class Raindrops {
-  convert = (n: number): string =>
-    factors
-      .map(({ factor, name }) => n % factor === 0 ? name : '')
-      .join('') || String(n)
+  convert(n: number): string {
+      return factors
+        .map((name, i) => n % i === 0 ? name : '')
+        .join('') || String(n)
+    }
 }
 
 export default Raindrops
