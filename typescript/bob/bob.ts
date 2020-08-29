ca28a50a88ddba576ed4807c314f3303f6822a00
Mauricio Robayo <rfmajo@gmail.com>
2020-08-28 19:59:51 -0500

    Use trim only when needed

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -9,33 +9,35 @@ type BobResponses =
 
 class Bob {
   hey(message: string): BobResponses {
-    const trimmedMessage = message.trim();
-
-    if (trimmedMessage.length === 0) {
+    if (this.isEmpty(message)) {
       return "Fine. Be that way!";
     }
 
-    if (this.isAskingLoudly(trimmedMessage)) {
+    if (this.isAskingLoudly(message)) {
       return 'Calm down, I know what I\'m doing!';
     }
 
-    if (this.isAsking(trimmedMessage)) {
+    if (this.isAsking(message)) {
       return "Sure.";
     }
 
-    if (this.isYelling(trimmedMessage, message)) {
+    if (this.isYelling(message)) {
       return "Whoa, chill out!";
     }
 
     return "Whatever.";
   }
 
-  private isYelling(trimmedMessage: string, message: string): boolean {
-    return trimmedMessage.toUpperCase() === message && /[a-z]/i.test(trimmedMessage);
+  private isEmpty(message: string): boolean {
+    return message.trim().length === 0;
+  }
+
+  private isYelling(message: string): boolean {
+    return message.toUpperCase() === message && /[a-z]/i.test(message);
   }
 
   private isAsking(message: string): boolean {
-    return message.endsWith("?");
+    return message.trim().endsWith("?");
   }
 
   private isAskingLoudly(message: string): boolean {
