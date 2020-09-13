ff30914fe46670b35daa79f42da8674250d006f1
Mauricio Robayo <rfmajo@gmail.com>
2020-09-12 19:20:37 -0500

    Better variable name

--- typescript/gigasecond/gigasecond.ts
+++ typescript/gigasecond/gigasecond.ts
@@ -2,10 +2,10 @@ const GIGASECOND = 1_000_000_000
 
 class Gigasecond {
   constructor(private initialDate: Date) {}
-  
+
   date(): Date {
-    const gigamilliseconds = GIGASECOND * 1000
-    return new Date(this.initialDate.getTime() + gigamilliseconds)
+    const milliseconds = GIGASECOND * 1000
+    return new Date(this.initialDate.getTime() + milliseconds)
   }
 }
 
