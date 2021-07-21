40b6a766b55db1e6e21d96246940d180686ab222
Mauricio Robayo <rfmajo@gmail.com>
2021-07-20 20:55:52 -0500

    Add initial Luhn solution
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/luhn/luhn.test.ts
+++ typescript/luhn/luhn.test.ts
@@ -5,43 +5,43 @@ describe("Luhn", () => {
     expect(Luhn.valid("1")).toBeFalsy();
   });
 
-  xit("a single zero is invalid", () => {
+  it("a single zero is invalid", () => {
     expect(Luhn.valid("0")).toBeFalsy();
   });
 
-  xit("a simple valid SIN that remains valid if reversed", () => {
+  it("a simple valid SIN that remains valid if reversed", () => {
     expect(Luhn.valid("059")).toBeTruthy();
   });
 
-  xit("a valid Canadian SIN", () => {
+  it("a valid Canadian SIN", () => {
     expect(Luhn.valid("055 444 285")).toBeTruthy();
   });
 
-  xit("invalid Canadian SIN", () => {
+  it("invalid Canadian SIN", () => {
     expect(Luhn.valid("055 444 286")).toBeFalsy();
   });
 
-  xit("invalid credit card", () => {
+  it("invalid credit card", () => {
     expect(Luhn.valid("8273 1232 7352 0569")).toBeFalsy();
   });
 
-  xit("valid strings with a non-digit included become invalid", () => {
+  it("valid strings with a non-digit included become invalid", () => {
     expect(Luhn.valid("055a 444 285")).toBeFalsy();
   });
 
-  xit("valid strings with punctuation included become invalid", () => {
+  it("valid strings with punctuation included become invalid", () => {
     expect(Luhn.valid("055-444-285")).toBeFalsy();
   });
 
-  xit("valid strings with symbols included become invalid", () => {
+  it("valid strings with symbols included become invalid", () => {
     expect(Luhn.valid("055£ 444$ 285")).toBeFalsy();
   });
 
-  xit("single zero with space is invalid", () => {
+  it("single zero with space is invalid", () => {
     expect(Luhn.valid(" 0")).toBeFalsy();
   });
 
-  xit("input digit 9 is correctly converted to output digit 9", () => {
+  it("input digit 9 is correctly converted to output digit 9", () => {
     expect(Luhn.valid("091")).toBeTruthy();
   });
 });
--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -1,21 +1,21 @@
-class Luhn {
-  static valid(number: string): boolean {
-    
-    const numberWithoutSpaces = number.replace(/ /g, '')
-    
-    if (!/^\d{2,}$/.test(numberWithoutSpaces)) {
-      return false
+export default class Luhn {
+  static valid(string: string): boolean {
+    const stringWithoutSpaces = string.replace(/\s/g, "");
+    if (stringWithoutSpaces.length <= 1) {
+      return false;
     }
-    
-    let result = numberWithoutSpaces.length % 2 === 0 ? 0 : Number(numberWithoutSpaces[0])
-    for (let i = numberWithoutSpaces.length - 2; i >= 0; i -= 2) {
-      result += Number(numberWithoutSpaces[i + 1])
-      const doubledNumber = Number(numberWithoutSpaces[i]) * 2
-      result += doubledNumber > 9 ? doubledNumber - 9 : doubledNumber
+
+    let total = 0;
+
+    for (const [index, digit] of Object.entries(stringWithoutSpaces)) {
+      if (Number(index) % 2 === 0) {
+        total += Number(digit);
+      } else {
+        const doubled = Number(digit) * 2;
+        total += doubled > 9 ? doubled - 9 : doubled;
+      }
     }
-    
-    return result % 10 === 0
+
+    return total % 10 === 0;
   }
 }
-
-export default Luhn
