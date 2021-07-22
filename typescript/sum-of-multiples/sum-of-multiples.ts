5963837f9af441cb0b1128a361f6ea4a4acafecf
Mauricio Robayo <rfmajo@gmail.com>
2021-07-21 19:31:44 -0500

    Add sum-of-multiples solution
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ typescript/sum-of-multiples/sum-of-multiples.ts
@@ -0,0 +1,18 @@
+function SumOfMultiples(multiples: number[]): { to: (upTo: number) => number } {
+  function to(upTo: number): number {
+    let sum = 0;
+
+    for (let i = 0; i < upTo; i++) {
+      const isMultiple = multiples.some((multiple) => i % multiple === 0);
+      if (isMultiple) {
+        sum += i;
+      }
+    }
+
+    return sum;
+  }
+
+  return { to };
+}
+
+export default SumOfMultiples;
