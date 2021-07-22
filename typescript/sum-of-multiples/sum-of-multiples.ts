6234e48338042e19a8613e292f509415dba43714
Mauricio Robayo <rfmajo@gmail.com>
2021-07-21 19:37:57 -0500

    chore: exclude 0 from loop
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/sum-of-multiples/sum-of-multiples.ts
+++ typescript/sum-of-multiples/sum-of-multiples.ts
@@ -2,7 +2,7 @@ function SumOfMultiples(multiples: number[]): { to: (upTo: number) => number } {
   function to(upTo: number): number {
     let sum = 0;
 
-    for (let i = 0; i < upTo; i++) {
+    for (let i = 1; i < upTo; i++) {
       const isMultiple = multiples.some((multiple) => i % multiple === 0);
       if (isMultiple) {
         sum += i;
