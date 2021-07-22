ef592145ebe729a928fb5b316a6be44db282d247
Mauricio Robayo <rfmajo@gmail.com>
2021-07-21 19:55:00 -0500

    Add constant for min
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/grains/grains.ts
+++ typescript/grains/grains.ts
@@ -1,22 +1,19 @@
-const TOTAL_SQUARES = 64;
+const MIN_SQUARES = 1;
+const MAX_SQUARES = 64;
 export default class Grains {
   static square(square: number): number {
-    if (square <= 0) {
-      throw new Error("Min square is 1");
+    if (square < MIN_SQUARES) {
+      throw new Error(`Min square is ${MIN_SQUARES}`);
     }
 
-    if (square > TOTAL_SQUARES) {
-      throw new Error(`Max square is ${TOTAL_SQUARES}`);
-    }
-
-    if (square === 1) {
-      return 1;
+    if (square > MAX_SQUARES) {
+      throw new Error(`Max square is ${MAX_SQUARES}`);
     }
 
     return Math.pow(2, square - 1);
   }
 
   static total(): number {
-    return Math.pow(2, TOTAL_SQUARES);
+    return Math.pow(2, MAX_SQUARES);
   }
 }
