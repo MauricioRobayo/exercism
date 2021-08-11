5b49d56a134c0e14512e000762391e24ec5b9f73
Mauricio Robayo <rfmajo@gmail.com>
2021-08-10 21:02:19 -0500

    Add roman-numerals solution
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/roman-numerals/roman-numerals.test.ts
+++ typescript/roman-numerals/roman-numerals.test.ts
@@ -31,67 +31,67 @@ describe("RomanNumerals", () => {
     expect(RomanNumerals.roman(6)).toEqual(expected);
   });
 
-  xit("9, being 10 - 1, is IX", () => {
+  it("9, being 10 - 1, is IX", () => {
     const expected = "IX";
     expect(RomanNumerals.roman(9)).toEqual(expected);
   });
 
-  xit("20 is two X's", () => {
+  it("20 is two X's", () => {
     const expected = "XXVII";
     expect(RomanNumerals.roman(27)).toEqual(expected);
   });
 
-  xit("48 is not 50 - 2 but rather 40 + 8", () => {
+  it("48 is not 50 - 2 but rather 40 + 8", () => {
     const expected = "XLVIII";
     expect(RomanNumerals.roman(48)).toEqual(expected);
   });
 
-  xit("49 is not 40 + 5 + 4 but rather 50 - 10 + 10 - 1", () => {
+  it("49 is not 40 + 5 + 4 but rather 50 - 10 + 10 - 1", () => {
     const expected = "XLIX";
     expect(RomanNumerals.roman(49)).toEqual(expected);
   });
 
-  xit("50 is a single L", () => {
+  it("50 is a single L", () => {
     const expected = "LIX";
     expect(RomanNumerals.roman(59)).toEqual(expected);
   });
 
-  xit("90, being 100 - 10, is XC", () => {
+  it("90, being 100 - 10, is XC", () => {
     const expected = "XCIII";
     expect(RomanNumerals.roman(93)).toEqual(expected);
   });
 
-  xit("100 is a single C", () => {
+  it("100 is a single C", () => {
     const expected = "CXLI";
     expect(RomanNumerals.roman(141)).toEqual(expected);
   });
 
-  xit("60, being 50 + 10, is LX", () => {
+  it("60, being 50 + 10, is LX", () => {
     const expected = "CLXIII";
     expect(RomanNumerals.roman(163)).toEqual(expected);
   });
 
-  xit("400, being 500 - 100, is CD", () => {
+  it("400, being 500 - 100, is CD", () => {
     const expected = "CDII";
     expect(RomanNumerals.roman(402)).toEqual(expected);
   });
 
-  xit("500 is a single D", () => {
+  it("500 is a single D", () => {
     const expected = "DLXXV";
     expect(RomanNumerals.roman(575)).toEqual(expected);
   });
 
-  xit("900, being 1000 - 100, is CM", () => {
+  it("900, being 1000 - 100, is CM", () => {
     const expected = "CMXI";
     expect(RomanNumerals.roman(911)).toEqual(expected);
   });
 
-  xit("1000 is a single M", () => {
+  it("1000 is a single M", () => {
     const expected = "MXXIV";
     expect(RomanNumerals.roman(1024)).toEqual(expected);
   });
 
-  xit("3000 is three M's", () => {
+  it("3000 is three M's", () => {
     const expected = "MMM";
     expect(RomanNumerals.roman(3000)).toEqual(expected);
   });
--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,9 +1,45 @@
+const romanNumbers = [
+  {
+    1: "I",
+    5: "V",
+    10: "X",
+  },
+  {
+    1: "X",
+    5: "L",
+    10: "C",
+  },
+  {
+    1: "C",
+    5: "D",
+    10: "M",
+  },
+  {
+    1: "M",
+  },
+];
+
 class RomanNumerals {
   static roman(number: number): string {
-    if (number >= 4) {
-      return `${"I".repeat(5 - number)}V`;
+    const numberString = String(number);
+    let result = "";
+
+    for (let i = 0; i < numberString.length; i++) {
+      const unit = numberString[i];
+      const roman = romanNumbers[numberString.length - i - 1];
+
+      if (Number(unit) >= 9) {
+        result += `${roman[1].repeat(10 - Number(unit))}${roman[10]}`;
+      } else if (Number(unit) > 5) {
+        result += `${roman[5]}${roman[1].repeat(Number(unit) - 5)}`;
+      } else if (Number(unit) >= 4) {
+        result += `${roman[1].repeat(5 - Number(unit))}${roman[5]}`;
+      } else {
+        result += roman[1].repeat(Number(unit));
+      }
     }
-    return "I".repeat(number);
+
+    return result;
   }
 }
 
