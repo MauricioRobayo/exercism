8068266371277ea77143a7c604907420bec7fb5d
Mauricio Robayo <rfmajo@gmail.com>
2021-08-14 20:15:41 -0500

    Fix algorithm
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -1,13 +1,18 @@
 export default class Luhn {
   static valid(string: string): boolean {
     const stringWithoutSpaces = string.replace(/\s/g, "");
+
     if (stringWithoutSpaces.length <= 1) {
+      return false;
+    }
+
+    if (/[^\d]/.test(stringWithoutSpaces)) {
       return false;
     }
 
     let total = 0;
 
-    for (let i = 0; i < stringWithoutSpaces.length; i++) {
+    for (let i = stringWithoutSpaces.length - 1; i >= 0; i--) {
       const digit = Number(stringWithoutSpaces[i]);
 
       if (i % 2 === 0) {
