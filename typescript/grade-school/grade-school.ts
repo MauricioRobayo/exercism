b2cffccfcd082b189a5dcbcdd65a5381df5818c8
Mauricio Robayo <rfmajo@gmail.com>
2021-10-24 20:34:15 -0500

    grade-school initial solution
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,28 +1,36 @@
 export class GradeSchool {
-  private _roster = new Map();
-  private _studentsList = new Set();
-  
-  roster() {
+  private _roster = new Map<number, string[]>();
+  private _studentsList = new Map<string, number>();
+
+  roster(): { [key: number]: string[] } {
     return Object.fromEntries(
       [...this._roster.entries()]
         .sort((a, b) => a[0] - b[0])
-        .map(([key, value]) => ([key, [...value]]))
+        .map(([key, value]) => [key, [...value]])
     );
   }
 
-  add(student: string, grade: number) {
+  add(student: string, grade: number): void {
     if (this._studentsList.has(student)) {
-      return;
+      const previousGrade = this._studentsList.get(student)!;
+
+      this._roster.set(
+        previousGrade,
+        this._roster.get(previousGrade)!.filter((s) => s !== student)
+      );
     }
 
     const students = this._roster.get(grade) || [];
-    students.push(student)
-    this._roster.set(grade, students.sort((a: string, b: string)=> a.localeCompare(b)));
-    this._studentsList.add(student);
+    students.push(student);
+    this._roster.set(
+      grade,
+      students.sort((a: string, b: string) => a.localeCompare(b))
+    );
+    this._studentsList.set(student, grade);
   }
 
-  grade(n: number) {
-    const students = this._roster.get(n) || [];
+  grade(n: number): string[] {
+    const students: string[] = this._roster.get(n) || [];
     return [...students];
   }
 }
