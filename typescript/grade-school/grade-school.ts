14602ceb16b24d68406528c92cba7230126dc97c
Mauricio Robayo <rfmajo@gmail.com>
2021-10-28 21:22:16 -0500

    Add grade-school solution
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,36 +1,38 @@
 export class GradeSchool {
-  private _roster = new Map<number, string[]>();
-  private _studentsList = new Map<string, number>();
+  private _roster: string[][] = [];
+  private _students: { [key: string]: number } = {};
 
   roster(): { [key: number]: string[] } {
-    return Object.fromEntries(
-      [...this._roster.entries()]
-        .sort((a, b) => a[0] - b[0])
-        .map(([key, value]) => [key, [...value]])
-    );
+    const result: { [key: number]: string[] } = {};
+
+    for (let i = 0; i < this._roster.length; i++) {
+      if (this._roster[i]) {
+        result[i] = [...this._roster[i]] || [];
+      }
+    }
+
+    return result;
   }
 
   add(student: string, grade: number): void {
-    if (this._studentsList.has(student)) {
-      const previousGrade = this._studentsList.get(student)!;
-
-      this._roster.set(
-        previousGrade,
-        this._roster.get(previousGrade)!.filter((s) => s !== student)
+    const currentGrade = this._students[student];
+    if (currentGrade !== undefined) {
+      const filteredStudents = this._roster[currentGrade].filter(
+        (s) => s !== student
       );
+      console.log({ filteredStudents });
+      this._roster[currentGrade] = filteredStudents;
     }
 
-    const students = this._roster.get(grade) || [];
-    students.push(student);
-    this._roster.set(
-      grade,
-      students.sort((a: string, b: string) => a.localeCompare(b))
-    );
-    this._studentsList.set(student, grade);
+    if (!this._roster[grade]) {
+      this._roster[grade] = [];
+    }
+    this._roster[grade].push(student);
+    this._roster[grade].sort();
+    this._students[student] = grade;
   }
 
   grade(n: number): string[] {
-    const students: string[] = this._roster.get(n) || [];
-    return [...students];
+    return [...(this._roster[n] || [])];
   }
 }
